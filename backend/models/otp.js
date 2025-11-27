3c1a8beb5a8303f09b8f457582703d5b392e84c3
Channu-22 <chaannusinnur22072002@gmail.com>
2025-11-27 19:45:17 +0530

    feat(add): otp model created

--- backend/models/otp.js
+++ backend/models/otp.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import mailSender from "../utils/mailSender";
+
 
 const otpSchema = new mongoose.Schema({
     email: {
@@ -19,5 +21,25 @@ const otpSchema = new mongoose.Schema({
     }
 
 });
+
+// A FUNCTION TO SEND MAILS
+async function sendVerificationEmail(email,otp){
+    try{
+        let mailResponse = await mailSender(
+            email, 
+            "Verification Email from studyNotion Project", 
+            `<h1>Your OTP is ${otp}</h1>`
+        );
+        console.log("Email sent successfully: ", mailResponse);
+
+    }catch(err){
+        console.log("error occured when sending mail: ", err.message)
+    }
+}
+
+otpSchema.pre("save",async function(next){
+    await sendVerificationEmail(this.email,this.otp);
+    next();
+})
 
 export default mongoose.model("OTP", otpSchema);
