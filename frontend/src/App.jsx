598c2402fe0da0f4bbfa2f181b04a1cc4bcc76fb
Channu-22 <chaannusinnur22072002@gmail.com>
2025-12-21 19:51:28 +0530

    feat: starting of frotend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <div className="bg-rich text-white min-h-screen">Home Page</div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        {/* section 1 */}
+        {/* section 2 */}
+        {/* section 3 */}
+        {/* section 4 */}
+    </div>
+  )
+}
+
+export default Home
