afaa74d90e15fefdd856642bf34d94cbd52876ea
Channu-22 <chaannusinnur22072002@gmail.com>
2026-01-08 04:09:03 +0530

    create signup and login component

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { NavbarLinks } from "../../data/navbar-links";
 import Logo from "../../assets/Logo/Logo-Full-Light.png";
+import { AiOutlineMenu, AiOutlineShoppingCart } from "react-icons/ai"
 import { Link, matchPath } from "react-router-dom";
 import { ChevronDown, ShoppingCart } from "lucide-react";
 import { useLocation } from "react-router-dom";
@@ -97,7 +98,7 @@ function Header() {
 
         {/* login-signup-dashboard */}
 
-        <div className="flex gap-x-4 items-center">
+        {/* <div className="flex gap-x-4 items-center">
           {user && user?.accountType == "Instructor" && (
             <Link to="/dashboard/cart" className="relative">
               <ShoppingCart />
@@ -121,7 +122,37 @@ function Header() {
           )}
 
           {token != null && <ProfileDropDown />}
+        </div> */}
+            <div className="hidden items-center gap-x-4 md:flex">
+          {user && user?.accountType !== ACCOUNT_TYPE.INSTRUCTOR && (
+            <Link to="/dashboard/cart" className="relative">
+              <AiOutlineShoppingCart className="text-2xl text-richblack-100" />
+              {totalItems > 0 && (
+                <span className="absolute -bottom-2 -right-2 grid h-5 w-5 place-items-center overflow-hidden rounded-full bg-richblack-600 text-center text-xs font-bold text-yellow-100">
+                  {totalItems}
+                </span>
+              )}
+            </Link>
+          )}
+          {token === null && (
+            <Link to="/login">
+              <button className="rounded-[8px] border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100">
+                Log in
+              </button>
+            </Link>
+          )}
+          {token === null && (
+            <Link to="/signup">
+              <button className="rounded-[8px] border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100">
+                Sign up
+              </button>
+            </Link>
+          )}
+          {token !== null && <ProfileDropdown />}
         </div>
+        <button className="mr-4 md:hidden">
+          <AiOutlineMenu fontSize={24} fill="#AFB2BF" />
+        </button>
       </div>
     </div>
   );
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
 
-function SignUp() {
+import signupImg from "../assets/Images/signup.webp"
+// import Template from "../components/core/Auth/Template"
+import Template from "../components/core/HomePage/Auth/Template"
+
+function Signup() {
   return (
-    <div>SignUp</div>
+    <Template
+      title="Join the millions learning to code with StudyNotion for free"
+      description1="Build skills for today, tomorrow, and beyond."
+      description2="Education to future-proof your career."
+      image={signupImg}
+      formType="signup"
+    />
   )
 }
 
-export default SignUp
+export default Signup
